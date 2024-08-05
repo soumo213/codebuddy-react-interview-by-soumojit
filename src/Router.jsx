be2785e020457b5fa04553cfcaa5b1d014d71657
soumo213 <soumojitdas1999@gmail.com>
2024-08-05 21:31:23 +0530

    Post List Api Inte. Done

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
+import AddPost from "./pages/AddPost";
 import Root from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/posts", element: <Posts /> },
+      { path: "/add-post", element: <AddPost /> },
     ],
   },
 ]);
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,40 +1,64 @@
-import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import { LoadingScreen } from "../components";
 
 const Posts = () => {
+  const [postList, setPostList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://codebuddy.review/posts");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setPostList(result.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <LoadingScreen />;
+
   return (
     <div className="rounded-lg bg-gray-50 p-7 text-gray-900 shadow-lg">
       <h1 className="mb-7 text-4xl font-bold">Posts</h1>
-      <Link to="/" className="mb-4 flex items-center text-blue-600 hover:underline">
-        <Icon icon="mdi:arrow-left" className="mr-2" />
-        Back to Home
-      </Link>
-
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <div className="rounded-lg bg-white p-7 shadow-lg">
-          <h2 className="text-2xl font-bold">Post 1</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem, quibusdam,
-            quos, voluptatum voluptas quod quas voluptates quia doloribus nobis voluptatibus. Quam,
-            voluptate voluptatum. Quod, voluptate? Quisquam, voluptate voluptatum.
-          </p>
-        </div>
-        <div className="rounded-lg bg-white p-7 shadow-lg">
-          <h2 className="text-2xl font-bold">Post 2</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem, quibusdam,
-            quos, voluptatum voluptas quod quas voluptates quia doloribus nobis voluptatibus. Quam,
-            voluptate voluptatum. Quod, voluptate? Quisquam, voluptate voluptatum.
-          </p>
-        </div>
-        <div className="rounded-lg bg-white p-7 shadow-lg">
-          <h2 className="text-2xl font-bold">Post 3</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem, quibusdam,
-            quos, voluptatum voluptas quod quas voluptates quia doloribus nobis voluptatibus. Quam,
-            voluptate voluptatum. Quod, voluptate? Quisquam, voluptate voluptatum.
-          </p>
-        </div>
+      <div className="flex items-center justify-between">
+        <Link to="/" className="flex items-center text-blue-600 hover:underline">
+          <Icon icon="mdi:arrow-left" className="mr-2" />
+          Back to Home
+        </Link>
+        <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          Add Post
+        </button>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {postList?.length > 0 &&
+          postList.map((post) => (
+            <div className="rounded-lg bg-white p-7 shadow-lg" key={post.id}>
+              <h2 className="flex items-center space-x-4 text-2xl font-bold">
+                <img
+                  src={post.avatar}
+                  alt={`${post.firstName} ${post.lastName}`}
+                  className="h-12 w-12 rounded-full object-cover"
+                />
+                <span>
+                  {post.firstName} {post.lastName}
+                </span>
+              </h2>
+              <p className="text-gray-700">{post.writeup}</p>
+              <img src={post.image} alt="" />
+            </div>
+          ))}
       </div>
     </div>
   );
