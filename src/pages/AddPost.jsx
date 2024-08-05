c990562dcb8689a5d1f4ba2bd95386c2778e9608
soumo213 <soumojitdas1999@gmail.com>
2024-08-06 00:57:09 +0530

    Redirect Url Updated

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -338,7 +338,7 @@ const AddPost = () => {
             },
             body: JSON.stringify(payload),
           });
-          
+
           const result = await response.json();
 
           if (result.message === "Success") {
@@ -364,7 +364,7 @@ const AddPost = () => {
   return (
     <div className="rounded-lg bg-gray-50 p-7 text-gray-900 shadow-lg">
       <h1 className="mb-7 text-4xl font-bold">Add Post</h1>
-      <Link to="/" className="mb-4 flex items-center text-blue-600 hover:underline">
+      <Link to="/posts" className="mb-4 flex items-center text-blue-600 hover:underline">
         <Icon icon="mdi:arrow-left" className="mr-2" />
         Back to Post
       </Link>
